47536a69e5bf9a807fdb1bfa378839500ec3a47a
roksana35 <roksana7235@gmail.com>
2024-06-05 22:16:21 +0600

    Remove .env file from tracking

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express =require('express')
 const app =express()
 const cors= require('cors');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-// const stripe= require('stripe')(process.env.STRIPE_PUBLISHED_KEY)
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const stripe= require('stripe')(process.env.JWT_SECRET_KEY)
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port=process.env.PORT||5000;
 
 //middlware
 app.use(cors());
 app.use(express.json());
 
-// uzK6xzrLHnoniRyF
-// TaskHive
+console.log("JWT_SECRET_KEY:", process.env.JWT_SECRET_KEY);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmgfwvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,6 +32,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+      res.send({ token }); // Ensure the token is sent as an object property
+    });
+
+    const verifyToken=(req,res,next)=>{
+      console.log('inside verify token',req.headers.authorization)
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'forbidden access'})
+      }
+      const token=req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.JWT_SECRET_KEY,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'forbidden access'})
+        }
+        req.decoded=decoded;
+        next()
+      })
+     
+      
+
+    }
+
+    // verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
     app.post('/users',async(req,res)=>{
       const user=req.body;
       const quary={email:user.email}
@@ -43,7 +78,7 @@ async function run() {
         // Assign coins based on role
     let coin = 0;
     if (user.role === 'worker') {
-      coin = 10;
+       coin = 10;
     } else if (user.role === 'taskcreator') {
       coin = 50;
     }
@@ -51,6 +86,48 @@ async function run() {
     // Add the coin property to the user object
     user.coin = coin;
       const result=await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.get('/users',verifyToken,verifyAdmin,async(req,res)=>{
+      const result=await userCollection.find({role:'worker'}).toArray()
+      res.send(result)
+    })
+
+
+    app.get('/users/admin/:email',verifyToken,verifyAdmin,async(req,res)=>{
+      const email =req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message:'unauthorized access'})
+      }
+      const quary={email:email}
+      const user =await userCollection.findOne(quary);
+      let admin=false
+      if(user){
+          admin=user?.role === 'admin'
+      }
+      res.send({admin})
+    })
+
+
+    app.patch('/users/admins/:id',verifyToken,verifyAdmin,async(req,res)=>{
+      const id=req.params.id;
+      const { role } = req.body;
+      const filter={_id:new ObjectId(id)}
+      const updatedDoc={
+        $set:{
+          role:role,
+        }
+      }
+      const result=await userCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+
+    })
+
+    app.delete('/users/:id',verifyToken,verifyAdmin,async(req,res)=>{
+      const id=req.params.id;
+      const quary={_id:new ObjectId(id)}
+      const result=await userCollection.deleteOne(quary);
       res.send(result)
     })
     // Send a ping to confirm a successful connection
@@ -67,5 +144,5 @@ app.get('/',(req,res)=>{
   res.send('taskhive is sitting')
 })
 app.listen(port,()=>{
-  console.log(`Bistro boss is sitting on port:${port}`)
+  console.log(`taskhive is sitting on port:${port}`)
 })
